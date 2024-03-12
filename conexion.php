177f5f36037f18cdf8e6d1cbda7c4897fffdd401
pierorado <43764514+pierorado@users.noreply.github.com>
2024-03-11 20:39:21 -0500

    conection railway

--- conexion.php
+++ conexion.php
@@ -1,11 +1,12 @@
 <?php
 
-    $host = "localhost";
-    $usuario = "root";
-    $password = "";
-    $dbnombre = "test";
+    $DB_HOST = $_ENV["DB_HOST"];//localhost
+    $DB_USER = $_ENV["DB_USER"];//root
+    $DB_PASSWORD = $_ENV["DB_PASSWORD"];
+    $DB_NAME = $_ENV["DB_NAME"];//test
+    $DB_PORT = $_ENV["DB_PORT"];//DELETE
 
-    $mysqli = new mysqli($host, $usuario, $password, $dbnombre);
+    $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
